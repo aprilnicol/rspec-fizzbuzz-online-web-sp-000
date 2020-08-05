9253cb6b122629d38580890cf8806c4304602b57
April Adams <illusionlace@gmail.com>
2020-08-05 15:02:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 % 5 == 0 
+  elsif int % 3 % 5 == 0
     return "FizzBuzz"
-  else 
+  else
     return nil
 
   end
